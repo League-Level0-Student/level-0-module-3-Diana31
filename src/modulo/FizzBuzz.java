f34354b65af6d54b7918a7243f399397717aa8cb
538751 <538751@0338S-S-R0B6H0F.sdcs.local>
2018-09-18 14:28:54 -0700

    fizz buzz in progress

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,6 +26,12 @@ package modulo;
 **/
 
 public class FizzBuzz {
-
+public static void main(String[] args) {
+	for (int i = 1; i <21 ; i++) {
+	if (i%==0 && 1%5==0) {
+		System.out.println("Fizzbuz");
+	}
+	}
+}
 }
 
